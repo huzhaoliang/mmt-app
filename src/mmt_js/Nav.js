3ff1b9fcfa91cea6d54212ca532d287c6c9900cc
William Zhou <scy_zhou@163.com>
2019-08-10 19:17:40 +0800

    William changes nav

--- src/mmt_js/Nav.js
+++ src/mmt_js/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-// import '../mmt_css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import '../img/favicon.ico'
 
 
@@ -14,13 +13,22 @@ class MMTNav extends React.Component
                 <Navbar.Brand href="/">
                     <img className="d-inline-block align-top" src="../img/favicon.ico" alt="汽车人才网"/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Nav className="mr-auto">
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+                <Nav>
                     <Nav.Link href="/" className="text-white">首页</Nav.Link>
                     <Nav.Link href="#" className="text-white">职位搜索</Nav.Link>
                 </Nav>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    
+                    <Nav className="ml-auto">
+                        <NavDropdown className="text-white" title="游客，你好！" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.2">个人中心</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">退出</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/" className="text-white">登入</Nav.Link>
+                        <Nav.Link href="#" className="text-white">注册</Nav.Link>
+                    </Nav>
                 </Navbar.Collapse>
                 </Navbar>
             </Container>
